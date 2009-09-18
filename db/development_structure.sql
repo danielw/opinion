7d23e291f18a270b92590060612d52d4ba1ef83b
Cody Fauser <codyfauser@gmail.com>
2009-09-18 19:52:49 -0300

    update development_structure.sql

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,34 +1,142 @@
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL auto_increment,
-  `title` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `forum_id` int(11) DEFAULT NULL,
+  `body` text,
+  `body_html` text,
+  `subtitle` varchar(255) DEFAULT NULL,
+  `access_level` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `index_categories_on_access_level` (`access_level`),
+  KEY `index_categories_on_area_id_and_title_dashed` (`forum_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `forums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `anonymous_posts` tinyint(1) DEFAULT '0',
+  `body` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `content_type` varchar(255) DEFAULT NULL,
+  `filename` varchar(255) DEFAULT NULL,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `width` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_images_on_parent_id` (`parent_id`),
+  KEY `index_images_on_post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `posts` (
-  `id` int(11) NOT NULL auto_increment,
-  `category_id` int(11) default NULL,
-  `title` varchar(255) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `body` text,
   `body_html` text,
-  `user_id` int(11) default NULL,
-  `created_at` datetime default NULL,
-  `type` varchar(255) default NULL,
-  `parent_id` int(11) default NULL,
-  PRIMARY KEY  (`id`)
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `forum_id` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT 'normal',
+  `edited_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_posts_on_category_id_and_type` (`category_id`,`type`),
+  KEY `index_posts_on_created_at` (`created_at`),
+  KEY `index_posts_on_parent_id_and_type` (`parent_id`,`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `schema_info` (
-  `version` int(11) default NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `login` varchar(255) default NULL,
-  `password` varchar(255) default NULL,
-  `name` varchar(255) default NULL,
-  `email` varchar(255) default NULL,
-  `url` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
+  `token` varchar(32) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `signature` text,
+  PRIMARY KEY (`id`),
+  KEY `index_users_on_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO schema_info (version) VALUES (3)
+INSERT INTO schema_migrations (version) VALUES ('1');
+
+INSERT INTO schema_migrations (version) VALUES ('10');
+
+INSERT INTO schema_migrations (version) VALUES ('11');
+
+INSERT INTO schema_migrations (version) VALUES ('12');
+
+INSERT INTO schema_migrations (version) VALUES ('13');
+
+INSERT INTO schema_migrations (version) VALUES ('14');
+
+INSERT INTO schema_migrations (version) VALUES ('15');
+
+INSERT INTO schema_migrations (version) VALUES ('16');
+
+INSERT INTO schema_migrations (version) VALUES ('17');
+
+INSERT INTO schema_migrations (version) VALUES ('18');
+
+INSERT INTO schema_migrations (version) VALUES ('19');
+
+INSERT INTO schema_migrations (version) VALUES ('2');
+
+INSERT INTO schema_migrations (version) VALUES ('20');
+
+INSERT INTO schema_migrations (version) VALUES ('20090907173425');
+
+INSERT INTO schema_migrations (version) VALUES ('21');
+
+INSERT INTO schema_migrations (version) VALUES ('22');
+
+INSERT INTO schema_migrations (version) VALUES ('23');
+
+INSERT INTO schema_migrations (version) VALUES ('24');
+
+INSERT INTO schema_migrations (version) VALUES ('25');
+
+INSERT INTO schema_migrations (version) VALUES ('26');
+
+INSERT INTO schema_migrations (version) VALUES ('27');
+
+INSERT INTO schema_migrations (version) VALUES ('28');
+
+INSERT INTO schema_migrations (version) VALUES ('29');
+
+INSERT INTO schema_migrations (version) VALUES ('3');
+
+INSERT INTO schema_migrations (version) VALUES ('30');
+
+INSERT INTO schema_migrations (version) VALUES ('31');
+
+INSERT INTO schema_migrations (version) VALUES ('32');
+
+INSERT INTO schema_migrations (version) VALUES ('33');
+
+INSERT INTO schema_migrations (version) VALUES ('4');
+
+INSERT INTO schema_migrations (version) VALUES ('5');
+
+INSERT INTO schema_migrations (version) VALUES ('6');
+
+INSERT INTO schema_migrations (version) VALUES ('7');
+
+INSERT INTO schema_migrations (version) VALUES ('8');
+
+INSERT INTO schema_migrations (version) VALUES ('9');
