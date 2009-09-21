94603a5b94653c31e344073634db9fc97568555b
Mark Dunkley <mark@jadedpixel.com>
2009-09-21 09:47:36 -0400

    Integrating the new wysiwyg form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
+  def use_tinymce
+      @content_for_tinymce = "" 
+      content_for :tinymce do
+        javascript_include_tag "tiny_mce/tiny_mce"
+      end
+      @content_for_tinymce_init = "" 
+      content_for :tinymce_init do
+        javascript_include_tag "mce_editor"
+      end
+  end
+  
   def recent_date(date)    
     today = z(Time.now.utc)    
     date  = z(date)
