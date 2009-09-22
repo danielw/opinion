5d37a183aceafadf9d3496a25911b4a268924516
Mark Dunkley <mark@jadedpixel.com>
2009-09-22 11:14:26 -0400

    Adding the styles for the sticky

---
+++ public/javascripts/mce_editor.js
@@ -0,0 +1,12 @@
+tinyMCE.init({
+    theme:"advanced",
+    mode:"textareas",
+    plugins: "safari",
+    theme_advanced_buttons1 : "bold,italic,code,link,bullist,numlist,,",
+    	theme_advanced_buttons2 : "",
+    	theme_advanced_buttons3 : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    
+    
+});
