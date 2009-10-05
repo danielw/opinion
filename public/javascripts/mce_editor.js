10a005aec3eb432852e56a076dd90ace918cc5d0
Mark Dunkley <mark@jadedpixel.com>
2009-10-05 17:30:29 -0400

    Removed code button from wysiwyg and fixed blockquote styling

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -6,7 +6,7 @@ tinyMCE.init({
   
   dialog_type : "modal",
 
-  theme_advanced_buttons1 : "bold,italic,bullist,numlist, blockquote,|,codebtn, code",
+  theme_advanced_buttons1 : "bold,italic,bullist,numlist, blockquote,|,codebtn",
   theme_advanced_buttons2 : "",
   theme_advanced_buttons3 : "",
   theme_advanced_toolbar_location : "top",
