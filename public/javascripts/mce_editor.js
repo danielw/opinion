acf6b22ae7c0a5718eac36ad52cb70c59e11f98e
Mark Dunkley <mark@jadedpixel.com>
2009-10-20 11:00:10 -0400

    wysiwyg: Added inline image functionality, added href links

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -3,12 +3,12 @@ tinyMCE.init({
   mode:"exact",
   elements: "comment_body",
   plugins: "safari,codebtn,inlinepopups,trailing",
-  
-  dialog_type : "modal",
 
-  theme_advanced_buttons1 : "bold,italic,bullist,numlist, blockquote,|,codebtn",
+  dialog_type : "modal",
+  theme_advanced_buttons1 : "bold,italic,bullist,numlist, blockquote, image,|,link, unlink, |,codebtn",
   theme_advanced_buttons2 : "",
   theme_advanced_buttons3 : "",
+  gecko_spellcheck : true,
   theme_advanced_toolbar_location : "top",
   theme_advanced_toolbar_align : "left",
   theme_advanced_statusbar_location : "bottom",
