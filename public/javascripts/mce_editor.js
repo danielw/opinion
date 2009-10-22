ed6152a059a1769f8dace504915dbd71071d8523
Mark Dunkley <mark@jadedpixel.com>
2009-10-22 12:55:36 -0400

    Reconfigured blockquote functionality

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -2,10 +2,10 @@ tinyMCE.init({
   theme:"advanced",
   mode:"exact",
   elements: "comment_body",
-  plugins: "safari,codebtn,inlinepopups,trailing",
+  plugins: "safari,codebtn,inlinepopups",
 
   dialog_type : "modal",
-  theme_advanced_buttons1 : "bold,italic,bullist,numlist, blockquote, image,|,link, unlink, |,codebtn" ,
+  theme_advanced_buttons1 : "bold,italic,blockquote,|,bullist,numlist,|,link, unlink, |,codebtn, image" ,
   theme_advanced_buttons2 : "",
   theme_advanced_buttons3 : "",
   gecko_spellcheck : true,
--- public/javascripts/tiny_mce/plugins/codebtn/js/code.js
+++ public/javascripts/tiny_mce/plugins/codebtn/js/code.js
@@ -3,7 +3,7 @@ CodeDialog = {
   insert: function() {
     var content = $('code').value;
     var out2 = tinyMCEPopup.editor.dom.encode(content);
-    var out = "<code>"+out2+"</code>";
+    var out = "<pre>"+out2+"</pre>";
 
     tinyMCEPopup.execCommand('mceInsertContent', false, out);
 //    tinyMCEPopup.editor.selection.setContent(out)
